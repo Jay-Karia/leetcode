ca85672ae735809ad8f1ea8bc9a96db870bf08c5
Jay-Karia <jay.sanjay.karia@gmail.com>
2025-04-01 14:57:43 +0530

    66-plus-one
    time: 0ms; memory: 54mb

---
+++ 66-plus-one.ts
@@ -0,0 +1,32 @@
+let counter = 0;
+
+function plusOne(digits: number[]): number[] {
+  let sum = digits;
+  // console.log(sum);
+
+  // Check if the last digit is < 9
+  if (digits[digits.length - 1] < 9) {
+    sum[digits.length - 1]++;
+    // Add counter number of digits
+    for (let i = 0; i < counter; i++)
+      sum.push(0);
+    counter = 0;
+    return sum;
+  }
+
+  if (sum.length === 1) {
+    // console.log(sum);
+    sum = [1, 0];
+    for (let i = 0; i < counter; i++)
+      sum.push(0);
+    counter = 0;
+    return sum;
+  }
+
+  sum[digits.length - 1] = 0;
+  counter ++;
+
+  sum.pop();
+
+  return plusOne(sum);
+};
