0fbab8495ebbc9840571981ff53c82c5f100dce9
Jay-Karia <jay.sanjay.karia@gmail.com>
2025-06-17 09:51:05 +0530

    21

---
+++ 21-merge-sorted-lists/21-merge-sorted-lists.ts
@@ -0,0 +1,43 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function mergeTwoLists(
+  list1: ListNode | null,
+  list2: ListNode | null
+): ListNode | null {
+  if (!list1) return list2;
+  if (!list2) return list1;
+
+  let dummy = new ListNode(0);
+  let current = dummy;
+
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      list2 = list2.next;
+    }
+    current = current.next;
+  }
+
+  current.next = list1 ? list1 : list2;
+
+  return dummy.next || null; // Return the merged list, or null if both lists were empty
+}
+
+console.log(
+  mergeTwoLists(
+    new ListNode(1, new ListNode(2, new ListNode(4))),
+    new ListNode(1, new ListNode(3, new ListNode(4)))
+  )
+);
+// console.log(mergeTwoLists(new ListNode(), new ListNode()));
+// console.log(mergeTwoLists(new ListNode(), new ListNode(0)));
