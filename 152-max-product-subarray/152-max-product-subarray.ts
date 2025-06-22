ab019ddbe2d39c9b621920cf03477dbe8511a15c
Jay-Karia <jay.sanjay.karia@gmail.com>
2025-06-22 10:32:51 +0530

    152 Max Product Subarray

--- 152-max-product-subarray/152-max-product-subarray.ts
+++ 152-max-product-subarray/152-max-product-subarray.ts
@@ -3,10 +3,24 @@ function maxProduct(nums: number[]): number {
   if (len === 1) return nums[0];
 
   let maxProduct = nums[0];
-  let currentProduct = nums[0];
-  let subArrayLength = len - 1;
 
-  return 1;
+  for (let i = 0, currentProduct = 1; i < len; i++) {
+    currentProduct *= nums[i];
+    maxProduct = Math.max(maxProduct, currentProduct);
+
+    if (currentProduct === 0) {
+      currentProduct = 1; // Reset current product if it becomes zero
+    }
+  }
+  for (let i = len - 1, currentProduct = 1; i >= 0; i--) {
+    currentProduct *= nums[i];
+    maxProduct = Math.max(maxProduct, currentProduct);
+
+    if (currentProduct === 0) {
+      currentProduct = 1; // Reset current product if it becomes zero
+    }
+  }
+  return maxProduct;
 };
 
 console.log(maxProduct([2, 3, -2, 4])); // Output: 6
